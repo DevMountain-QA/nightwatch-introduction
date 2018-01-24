e2ce555583526a90a926c915c9bf216b6add3e2c
AJ Larson <aj.larson@devmounta.in>
2018-01-24 01:53:27 -0700

    added step5

---
+++ nightwatch/step5/supporting/data.js
@@ -0,0 +1,18 @@
+module.exports = {
+    simpleAddition : {
+        buttons: ['2', '+', '2', '='],
+        solution: '4'
+    },
+    decimalMultiplication : {
+        buttons: ['3', '2', '.', '1', '*', '2', '='],
+        solution: '64.2'
+    },
+    otherButtons : {
+        buttons: ['5', '4', '2', '1', '%', '+/-', '+', '2', '='],
+        solution: '-52.21'
+    },
+    simpleDivision: {
+        buttons: ['3', '5', '/', '7', '='],
+        solution: '5'
+    }
+}
---
+++ nightwatch/step5/tests/tests.js
@@ -0,0 +1,18 @@
+const selectors = require('../supporting/selectors')
+const functions = require('../supporting/functions')
+const data = require('../supporting/data')
+
+module.exports = {
+    beforeEach : browser => {
+        browser.url('http://localhost:3000')
+        functions.buttonClicker(browser, 'AC')
+    },
+    after : browser => {
+        browser.end()
+    },
+    'UI Check' : browser => functions.uiChecker(browser),
+    '2+2=4' : browser => functions.testRunner(browser, data.simpleAddition),
+    '32.1*2=64.2' : browser => functions.testRunner(browser, data.decimalMultiplication),
+    'otherButtons' : browser => functions.testRunner(browser, data.otherButtons),
+    '35/7=5' : browser => functions.testRunner(browser, data.simpleDivision)
+}
